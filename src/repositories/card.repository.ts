ac0d14e5313376c11e102ff51d7e64665a77c51e
Arthur Brito <arthurlwj@gmail.com>
2025-09-18 23:09:09 -0300

    feat: Primeira parte dos filtros de busca

--- src/repositories/card.repository.ts
+++ src/repositories/card.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateCardDto, UpdateCardDto } from "src/dto/cardDto";
+import { FilterCardDTO } from "src/dto/filter-cardDto";
 import { ExpensesCreditCard } from "src/expenses/card.entity";
 import { Repository } from "typeorm";
 
@@ -27,6 +28,24 @@ export class ExpenseCreditCardRepository {
     async cardListById(id: string): Promise<CreateCardDto | null> {
         return this.repo.findOne({ where: { id } })
     }
+
+    async fieldFilter(data: FilterCardDTO) {
+        const query = this.repo.createQueryBuilder('card');
+
+        if(data.cardName) query.andWhere('card.cardName = :cardName', {cardName: data.cardName});
+        if(data.cardLimit) query.andWhere('card.cardLimit = :cardLimit', {cardLimit: data.cardLimit});
+        if(data.dueDay) query.andWhere('card.dueDay = :dueDay', {dueDay: data.dueDay});
+
+        return query.getMany();
+    }
+
+
+
+
+
+
+
+
 
     async cardUpdate(id: string, data: UpdateCardDto) {
         return this.repo.update({ id }, data)
